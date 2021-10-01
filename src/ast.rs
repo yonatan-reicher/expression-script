53f619519ccc1bc75170a43ce906fb2bfe92fa7c
Yonatan Reicher <yony252525@gmail.com>
2021-10-01 09:46:08 +0300

    Derived clone

--- src/ast.rs
+++ src/ast.rs
@@ -1,12 +1,12 @@
 use std::collections::hash_map::HashMap;
 use std::rc::Rc;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Ident {
     pub name: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expr {
     Var(Ident),
     Func {
