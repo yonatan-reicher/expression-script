4c45aeda0de5cd5fc7721bfebb4c8b25d3c977d9
Yonatan Reicher <yony252525@gmail.com>
2021-10-06 21:37:21 +0300

    repl mode

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,47 @@ mod parse;
 mod ast;
 mod reduce;
 
-fn main() {
-    let code = "galord123 -> guyyyy";
-    match parse::parse(code) {
-        Ok(x) => {
-            println!("Parsing successful!");
-            println!("{:?}", x);
-        },
-        Err(()) => {
-            println!("Failed parsing");
-        },
-    };
+
+mod repl {
+    use std::io::*;
+    use super::*;
+    use crate::reduce::reduce;
+
+    fn read_line(stream: Stdin) -> Result<Option<String>> {
+        let mut buf = String::new();
+        Ok(if let 0 = stream.read_line(&mut buf)? { None } else {
+            if buf.ends_with("\r\n") { buf.truncate(buf.len() - 2); }
+            else if buf.ends_with("\n") || buf.ends_with("\r") { buf.truncate(buf.len() - 1); }
+            Some(buf)
+        })
+    }
+
+    pub fn repl() -> std::io::Result<()> {
+        loop {
+            print!("> ");
+            stdout().flush();   //  flush the output nessecery after print!
+            match read_line(stdin())? {
+                None => panic!(),
+                Some(string) if string.trim() == "q" => break,
+                Some(code) => {
+                    match parse::parse(&code) {
+                        Ok(x) => {
+                            println!("Parsing successful!");
+                            println!("tree: \n{:?}", x);
+                            println!("reduced: \n{:?}", reduce(&x));
+                        },
+                        Err(()) => {
+                            eprintln!("Failed parsing");
+                        },
+                    };
+                },
+            }
+        };
+        Ok(())
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    repl::repl()?;
+    Ok(())
 }
